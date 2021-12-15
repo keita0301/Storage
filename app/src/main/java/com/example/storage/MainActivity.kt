f59949f22f7b77f5b9d1b1235e0d188bed9e4b1e
keita0301 <x19g032@mitsuhashi.ac.jp>
2021-12-15 10:10:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/storage/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.storage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.storage.FirebaseStorage
+import org.w3c.dom.Text
+
+//import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val test : TextView = findViewById(R.id.test)
+        Log.d("Test","1")
+        val storage = FirebaseStorage.getInstance()
+        val storageRef = storage.reference
+        val dir = storageRef.child("neet.png")
+        Log.d("Test","2")
+        val byteLength=1024*1024L
+        dir.getBytes(byteLength).addOnSuccessListener{
+                bytes ->
+            Log.d("Test","3")
+            val data = String(bytes, Charsets.UTF_8)
+            val array=data.split("/n")
+            var res=""
+            val i=1;
+            Log.d("Test","4")
+            for(item in array){
+                res=i.toString().plus(item).plus("\n")
+            }
+            test.text=res
+        }.addOnFailureListener {  }
+    }
+}
