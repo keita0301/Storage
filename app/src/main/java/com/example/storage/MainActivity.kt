4a8f8ec21ec2e514a93363244dc0249275a13926
keita <x19g032@mitsuhashi.ac.jp>
2021-12-15 10:49:46 +0900

    a

--- app/src/main/java/com/example/storage/MainActivity.kt
+++ app/src/main/java/com/example/storage/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.storage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import com.google.firebase.storage.FirebaseStorage
 import org.w3c.dom.Text
@@ -14,27 +15,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val test : TextView = findViewById(R.id.test)
-        Log.d("Test","1")
-        val storage = FirebaseStorage.getInstance()
-        val storageRef = storage.reference
-        val gsReference = storage.getReferenceFromUrl("gs://test02-4d86d.appspot.com/neet.png")
-
+//        val test : TextView = findViewById(R.id.test)
+//        val image : ImageView = findViewById(R.id.image)
+//        Log.d("Test","1")
+//        val storage = FirebaseStorage.getInstance()
+//        val storageRef = storage.reference
+//        val gsReference = storage.getReferenceFromUrl("gs://test02-4d86d.appspot.com/neet.png")
+//
 //        val dir = storageRef.child("neet.png")
-        Log.d("Test","2")
-        val byteLength=1024*1024L
-        gsReference.getBytes(byteLength).addOnSuccessListener{
-                bytes ->
-            Log.d("Test","3")
-            val data = String(bytes, Charsets.UTF_8)
-            val array=data.split("/n")
-            var res=""
-            val i=1;
-            Log.d("Test","4")
-            for(item in array){
-                res=i.toString().plus(item).plus("\n")
-            }
-            test.text=res
-        }.addOnFailureListener {  }
+//        Log.d("Test","2")
+//        val byteLength=1024*1024L
+//        dir.getBytes(byteLength).addOnSuccessListener{
+//                bytes ->
+//            Log.d("Test","3")
+//            val data = String(bytes, Charsets.UTF_8)
+//            val array=data.split("/n")
+//            var res=""
+//            val i=1;
+//            Log.d("Test","4")
+//            for(item in array){
+//                res=i.toString().plus(item).plus("\n")
+//            }
+//            image.setImage
+//            test.text=res
+//        }.addOnFailureListener {  }
     }
 }
