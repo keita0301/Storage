8a9d00cf9450668b2e9a48460733db2be04bf2d0
keita0301 <x19g032@mitsuhashi.ac.jp>
2021-12-21 12:56:20 +0900

    storage

--- app/src/main/java/com/example/storage/MainActivity.kt
+++ app/src/main/java/com/example/storage/MainActivity.kt
@@ -1,22 +1,39 @@
 package com.example.storage
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.google.firebase.storage.FirebaseStorage
+import android.content.ContentResolver
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_GALLERY_TAKE = 2
+    private val RECORD_REQUEST_CODE = 42
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val image : ImageView = findViewById(R.id.image)
-        val storageRef = FirebaseStorage.getInstance().reference
-        val imageRef = storageRef.child("neet.png")
-            Glide.with(this)
-            .load(imageRef)
-            .into(image)
+        val button : Button = findViewById(R.id.button)
+        val imageView : ImageView = findViewById(R.id.imageView)
+
+        button.setOnClickListener{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST_GALLERY_TAKE)
+        }
+
+//        val image : ImageView = findViewById(R.id.image)
+//        val storageRef = FirebaseStorage.getInstance().reference
+//        val imageRef = storageRef.child("neet.png")
+//        Glide.with(this).load(imageRef).into(image)
 
 
 //        val test : TextView = findViewById(R.id.test)
@@ -43,5 +60,25 @@ class MainActivity : AppCompatActivity() {
 //            image.setImage
 //            test.text=res
 //        }.addOnFailureListener {  }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_GALLERY_TAKE && resultCode == Activity.RESULT_OK) {
+            if (data != null){
+                val uri = data.data
+                imageView.setImageURI(uri)
+
+                val fileName= UUID.randomUUID().toString() + ".jpg"
+                val refStorage= FirebaseStorage.getInstance().reference.child("/images/$fileName")
+                refStorage.putFile(uri!!)
+                    .addOnSuccessListener {
+                        Log.d("nakayama", "image uploaded")
+                    }
+                    .addOnFailureListener {
+                        Log.d("RegisterActivity", "${it.message}")
+                    }
+            }
+        }
     }
 }
